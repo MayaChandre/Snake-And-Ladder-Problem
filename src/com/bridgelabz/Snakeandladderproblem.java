c8ee6c24cdeb9229c1aefa9e79c9b47179cefb3e
mayavathi <mayachandre27@gmail.com>
2021-06-22 09:11:27 -0400

    Rolls a die

--- src/com/bridgelabz/Snakeandladderproblem.java
+++ src/com/bridgelabz/Snakeandladderproblem.java
@@ -5,9 +5,16 @@ public class SnakeAndladderproblem
 //UC1 Initializing Starting position of game
 	static final int STARTING_POSITION = 0;
 	static final int ENDING_POSITION = 100;
+	public void RollingDice()
+	{
+		Random random = new Random();
+		int DICE = (random.nextInt(6)+1);
+		System.out.println("Rolling Dice :"+DICE);
+	}
+
 	public static void main(String args[])
 	{
-//Main method
-	SnakeAndLadder player = new SnakeAndLadder();
+		SnakeAndladderproblem Game1 = new snakeandladder();
+		Game1.RollingDice();
 	}
 }
