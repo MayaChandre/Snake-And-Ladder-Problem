bce09ed6c1a54cd636bd2e19d84189067ffc7788
mayavathi <mayachandre27@gmail.com>
2021-06-23 04:08:29 -0400

    total dice count

--- src/com/bridgelabz/Snakeandladderproblem.java
+++ src/com/bridgelabz/Snakeandladderproblem.java
@@ -9,44 +9,39 @@ public class SnakeAndladderproblem
 	{
 	//UC2 Initializing Rolling Dice
 		Random random = new Random();
-	int currentPosition = STARTING_POSITION ;
-	// UC4 while loop for repet till player won
+     	int currentPosition = STARTING_POSITION ;
+     	int total_dice_count = 0;
+	// UC4 while loop for repeat till player won
 			System.out.println("Game Played by the player");
 			System.out.println(" ");
 			while( currentPosition < ENDING_POSITION )
 			{
 				int DICE = (random.nextInt(6)+1);
+				total_dice_count++;
+				
 
 //UC3 defining Snake, Ladder or no play
 	int action = random.nextInt(3);
-	
-	//System.out.println("Rolling Dice : "+DICE);
-	//System.out.println("Action perform :"+action);
 	if(action == 1 && (currentPosition + DICE <= ENDING_POSITION))
 	{
-
-			currentPosition = currentPosition + DICE;
-			//System.out.println("Player moves for Ladder");
+		currentPosition = currentPosition + DICE;
 	}
 	else if(action == 2 && (currentPosition - DICE >= STARTING_POSITION))
 	{
 
 			currentPosition = currentPosition - DICE;
-			//System.out.println("Player moves for Snake");
 	}
 	else
 	{
-			// System.out.println("No Play");
-	}
-	//UC5 Player reaches at exact winning position 
+	} 
 	
-
-	System.out.println("Dice : "+DICE+" | Current Position : "+ currentPosition);
+System.out.println("Dice : "+DICE+" | Current Position : "+ currentPosition);
 
 }
 			System.out.println(" ");
-
-	}
+			//UC6 total times dice throws
+			System.out.println("Total Count of Dice Throws : "+total_dice_count);
+		}
 
 	public static <SnakeAndLadder> void main(String args[])
 	{
