1a7ce37684dadd965efe45f66807223a37a8c4fa
mayavathi <mayachandre27@gmail.com>
2021-06-23 03:56:46 -0400

    gets exact winning position

--- src/com/bridgelabz/Snakeandladderproblem.java
+++ src/com/bridgelabz/Snakeandladderproblem.java
@@ -5,42 +5,52 @@ public class SnakeAndladderproblem
 //UC1 Initializing Starting position of game
 	static final int STARTING_POSITION = 0;
 	static final int ENDING_POSITION = 100;
-	public static void GamePlayed()
+	public void GamePlayed()
 	{
 	//UC2 Initializing Rolling Dice
-	Random random = new Random();
-	int current_position;
-	int DICE = (random.nextInt(6)+1);
+		Random random = new Random();
+	int currentPosition = STARTING_POSITION ;
+	// UC4 while loop for repet till player won
+			System.out.println("Game Played by the player");
+			System.out.println(" ");
+			while( currentPosition < ENDING_POSITION )
+			{
+				int DICE = (random.nextInt(6)+1);
+
 //UC3 defining Snake, Ladder or no play
 	int action = random.nextInt(3);
-	current_position = STARTING_POSITION;
-	System.out.println("Rolling Dice : "+DICE);
-	System.out.println("Action perform :"+action);
-	if(action == 1 && (current_position + DICE <= ENDING_POSITION))
+	
+	//System.out.println("Rolling Dice : "+DICE);
+	//System.out.println("Action perform :"+action);
+	if(action == 1 && (currentPosition + DICE <= ENDING_POSITION))
 	{
 
-			current_position = current_position + DICE;
-			System.out.println("Player moves for Ladder");
+			currentPosition = currentPosition + DICE;
+			//System.out.println("Player moves for Ladder");
 	}
-	else if(action == 2 && (current_position - DICE >= STARTING_POSITION))
+	else if(action == 2 && (currentPosition - DICE >= STARTING_POSITION))
 	{
 
-			current_position = current_position - DICE;
-			System.out.println("Player moves for Snake");
+			currentPosition = currentPosition - DICE;
+			//System.out.println("Player moves for Snake");
 	}
 	else
 	{
-			 System.out.println("No Play");
+			// System.out.println("No Play");
 	}
-	
-	System.out.print(" "+current_position);
-
-	}
+	//UC5 Player reaches at exact winning position 
 	
 
-      public static void main(String args[])
-{
-	SnakeAndladderproblem	Player = new SnakeAndladderproblem();	
-	SnakeAndladderproblem.GamePlayed();
+	System.out.println("Dice : "+DICE+" | Current Position : "+ currentPosition);
+
 }
+			System.out.println(" ");
+
+	}
+
+	public static <SnakeAndLadder> void main(String args[])
+	{
+		SnakeAndladderproblem	Player = new SnakeAndladderproblem();	
+		Player.GamePlayed();
+	}
 }
